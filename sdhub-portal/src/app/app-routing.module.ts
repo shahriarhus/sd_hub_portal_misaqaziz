24ddad7a794fde36c3c76e7a901283ad4caa04f8
Shahriar Hussain <shahriarhus@gmail.com>
2024-10-15 19:40:59 +0530

    Update app-routing.module.ts

--- sdhub-portal/src/app/app-routing.module.ts
+++ sdhub-portal/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { SigninComponent } from './signin/signin.component';
 import { RegisterComponent } from './register/register.component';
 import { RegisterEducatinComponent } from './register/register-education/register-educatin.component';
 import { RegisterDeclarationComponent } from './register/register-declaration/register-declaration.component';
+import { RegisterSingleComponent } from './register-single/register-single.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: "full" },
   { path: 'signin', component: SigninComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'register-declaration', component: RegisterDeclarationComponent},
-  { path: 'register-education', component: RegisterEducatinComponent}
+  { path: 'register-education', component: RegisterEducatinComponent},
+  { path: 'register-single', component:RegisterSingleComponent}
 ];
 
 @NgModule({
