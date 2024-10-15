77bbf990413511dd06e3263360c3dab865aaeb8d
Shahriar Hussain <shahriarhus@gmail.com>
2024-10-15 19:45:08 +0530

    Add demo stepper
    From https://material.angular.io/components/stepper/overview

--- sdhub-portal/src/app/register-single/register-single.component.ts
+++ sdhub-portal/src/app/register-single/register-single.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-register-single',
   templateUrl: './register-single.component.html',
-  styleUrl: './register-single.component.css'
+  styleUrl: './register-single.component.css',
+  standalone: false
 })
 export class RegisterSingleComponent {
+  private _formBuilder = inject(FormBuilder);
 
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
 }
